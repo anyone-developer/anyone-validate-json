a73be078fc2c83eb634d88dec5cb6fc44ac10aa7
nzhang4 <nzhang4@stubhub.com>
2020-11-12 14:53:39 +0800

    first commit

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+import ArgsModel = require('./src/types/args');
+import AvjItemModel = require('./src/types/avj-item');
+
+export type Args = ArgsModel;
+export type AvjItem = AvjItemModel;
---
+++ index.ts
@@ -0,0 +1,3 @@
+import avj from './src/avj';
+
+export default avj;
